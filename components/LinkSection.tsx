6bf75bc080e179a3c9582fac7045bc3e1fd32e99
Leonardo <leonardocsbloise@gmail.com>
2024-02-29 16:33:19 -0300

    fix: adding key

--- components/LinkSection.tsx
+++ components/LinkSection.tsx
@@ -11,8 +11,8 @@ export default function LinkSection({ links }: Props) {
         <section className="w-full h-full p-4">
             <ul className="flex flex-wrap h-5/6 overflow-y-scroll">
                 {links.map(link => (
-                    <li className="ml-8 mt-8">
-                        <div className="w-[200px] p-2 flex flex-col items-center border-[1px] dark:bg-[#222222] dark:border-none dark:text-white cursor-pointer rounded-lg p-4">
+                    <li key={link.anchor} className="ml-8 mt-8">
+                        <div className="w-[200px] flex flex-col items-center border-[1px] dark:bg-[#222222] dark:border-none dark:text-white cursor-pointer rounded-lg p-4">
                             <Link href={link.anchor} className="flex flex-col items-center" target="_blank">
                                 <Image src={link.symbol} alt="Alternative" className="w-[100px] h-[100px]"/>
                                 <span className="mt-4">{link.name}</span>
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -15,7 +15,7 @@ export default function Menu({ items }: Props) {
         <nav className="bg-[#F5F5F5] dark:bg-[#444444] dark:border-[#777777] fixed bottom-10 p-4 w-1/2 left-1/2 -translate-x-1/2 rounded-lg border-[1px] flex ">
             <ul className="flex w-full space-x-4">
                 {items.map(item => (
-                    <li title={item.title} className="parent" >
+                    <li key={item.href} title={item.title} className="parent" >
                         <Link href={item.href} title={item.title} >
                             <Image className={activePathname == item.href ? 'animate-bounce' : 'childElement'} src={item.src} width={40} height={40} alt={item.alt} />
                         </Link>
